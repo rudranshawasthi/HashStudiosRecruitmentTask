5900d961a353118d60ad04eac5acaa7c1dde925a
rudranshawasthi <rudranshofficial@gmail.com>
2020-11-07 02:55:49 +0530

    basic design

---
+++ RecruitmentTask/lib/widget/imgbox.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ImgBox extends StatelessWidget {
+  final imglist;
+  final index;
+  final onclicCallback;
+
+  ImgBox({this.imglist, this.index, this.onclicCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: onclicCallback,
+      child: Container(
+        width: 60,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(imglist[index]),
+              fit: BoxFit.fill,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            )),
+      ),
+    );
+  }
+}
